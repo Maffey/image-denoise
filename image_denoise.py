d5ab6a92d119247f12b5eba8f2920fa7baca63a4
maffey <matiusz98@gmail.com>
2020-05-19 19:19:49 +0200

    doc polish

--- image_denoise.py
+++ image_denoise.py
@@ -1,23 +1,5 @@
 #! python3
-"""Based on 'n' numbers of matrices, create an average matrix of them (image denoise).
-
-File with data is given in a .txt file.
-First line is the number of rows and columns in matrices (their size) separated by 'x'.
-Following lines are matrices separated by spaces, one line for each matrix.
-If the calculations succeed, script appends the result at the end of the file.
-
-Running the program displays in the terminal bunch of useful information for the calculations
-such as average, standard deviation and error for each pixel calculated.
-
-The program can run in two modes:
-* standard - where the images are assumed to be of the same quality.
-* weighted - where each image was created differently.
-
-To run in the 'weighted' mode, append `-w` flag and the path to the file
-with weights (yes, weights must be calculated by hand).
-
-To use the script, type: `python image_denoise.py path/to/matrices_file.txt [-w path/to/weight_file.txt]`
-"""
+"""Based on 'n' numbers of matrices, create an average matrix of them (image denoise)."""
 
 import argparse
 import math
